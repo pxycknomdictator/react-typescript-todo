8d8563f2943098702203b7a48de85d04a3361cd8
Noman <pxycknomdictator@outlook.com>
2024-10-26 20:10:11 +0500

    added components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { Clear } from "./components/Clear.tsx";
+import { Input } from "./components/Input.tsx";
+import { Update } from "./components/Update.tsx";
+import { TodoList } from "./components/TodoList.tsx";
+
 export const App = () => {
-  return <div className="text-3xl text-center underline">App</div>;
+  return (
+    <main className="w-screen min-h-screen flex items-center justify-center bg-[#f2f4f7]">
+      <section className="w-full h-screen c_small:h-auto py-9 c_small:p-7 mx-auto c_small:w-[90%] sm:w-10/12 c_medium:w-[70%] lg:max-w-[750px] bg-white p-4 sm:pt-9 c_small:pb-3 rounded-lg custom_shadow relative">
+        <Input />
+        <Update />
+        <TodoList />
+        <Clear />
+      </section>
+    </main>
+  );
 };
